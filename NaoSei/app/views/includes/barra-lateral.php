368303baa4b11c0860d05934cd3274a74d7212d4
Matheus <matheusviana3038@gmail.com>
2025-12-14 15:07:19 -0300

    Um commit qualquer

---
+++ NaoSei/app/views/includes/barra-lateral.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../../../public/assets/css/index.css">
+</head>
+<body>
+    <header class="barra-lateral">
+        <div class="btn-fechar">
+            <button type="button">x</button>
+        </div>
+        <nav>
+            <a href="">Teste</a>
+            <a href="">Teste</a>
+            <a href="">Teste</a>
+        </nav>
+    </header>
+</body>
+</html>
---
+++ NaoSei/public/assets/js/index.js
@@ -0,0 +1,5 @@
+function modalNovo(){
+    const modal = document.querySelector(".modal-novo");
+
+    modal.classList.toggle("ativo");
+}
