ee98d7c7125dd0822283033707197ea0bde91c6f
Matheus <matheusviana3038@gmail.com>
2026-01-13 13:44:01 -0300

    Terminei de criar as funcoes que listam as tarefas, e fiz uns ajustes de estilo

--- atividades/miniprojetojs/index.js
+++ atividades/miniprojetojs/index.js
@@ -7,3 +7,31 @@ function fecharModal(){
     criarTarefa.classList.remove("active");
     overlay.classList.remove("active");
 }
+
+function buscarTarefas(){
+    fetch("http://localhost:3000/tarefas")
+    .then(res => res.json())
+    .then(data => {
+        inserirTarefas(data);
+    })
+}   
+
+function inserirTarefas(listaTarefas){
+    if(listaTarefas.length > 0){
+        listaTarefas.forEach(tarefa => {
+            let li = document.createElement("li");
+            li.innerHTML = `   
+                    <div class="info">          
+                        <h4>${tarefa.titulo}</h4>
+                        <p>${tarefa.descricao}</p>
+                    </div>
+                    <div class="btn-apagar">
+                        <box-icon type='solid' name='trash'></box-icon>
+                    </div>
+                    `
+            lista.appendChild(li);
+        });
+    }
+}
+
+buscarTarefas();
